4b3ee0dbe9862a765f99ee9266313a4452da1951
Mozzarella Dev <84394401+Mozzarella-dev@users.noreply.github.com>
2021-09-21 09:44:39 +0200

    started developing

---
+++ config/database.py
@@ -0,0 +1,32 @@
+# config/database.py
+DATABASES = {
+  "default": "mysql",
+  "mysql": {
+    "host": "127.0.0.1",
+    "driver": "mysql",
+    "database": "masonite",
+    "user": "root",
+    "password": "",
+    "port": 3306,
+    "log_queries": False,
+    "options": {
+      #  
+    }
+  },  
+  "postgres": {
+    "host": "127.0.0.1",
+    "driver": "postgres",
+    "database": "masonite",
+    "user": "root",
+    "password": "",
+    "port": 5432,
+    "log_queries": False,
+    "options": {
+      #  
+    }
+  },
+  "sqlite": {
+    "driver": "sqlite",
+    "database": "masonite.sqlite3",
+  }
+}
