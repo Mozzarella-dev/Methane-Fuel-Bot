e625a57897836d77099bfa748785ffea7cdc4fb6
Mozzarella Dev <84394401+Mozzarella-dev@users.noreply.github.com>
2021-09-21 09:44:50 +0200

    started dev

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging
 
-updater = Updater(token='TOKEN', use_context=True)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+updater = Updater(token='2042025216:AAFB9nm1kaw4CRP3Ft0rv_kgcgoHucmEkPA', use_context=True)
+
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
