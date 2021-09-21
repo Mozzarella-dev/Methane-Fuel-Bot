fbb112ad122b6cc1ac4fdddf85a2199764643e54
Mozzarella Dev <84394401+Mozzarella-dev@users.noreply.github.com>
2021-09-21 09:48:19 +0200

    added todos

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
+# TODO: usare database per utente
+# TODO: comando /settings per modificare i valori come Km or Miles, EUR o USD
+# TODO: comando /month per vedere la media di questo mese con grafico
+# TODO: comando /week per vedere la media di questa settimana con grafico
+# TODO: comando /year per vedere la media dell'ultimo anno con grafico
+# TODO: comando /alltime per vedere tutto con grafico
+# TODO: trovare modo di fare grafici
+# TODO: comando /add per aggiungere km totali della macchina e costo pieno di metano
+# TODO: /info comando per spiegare il funzionamento del bot
+# TODO: /about me comando per il mio github
